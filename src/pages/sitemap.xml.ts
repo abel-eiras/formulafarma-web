9ca07b72d69a7070d110057a0f227b3eed7659fe
Abel Eiras <abel.eiras@hotmail.com>
2025-11-23 18:35:48 +0100

    feat: implementar SEO básico (Open Graph, meta tags, sitemap.xml, structured data)

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,48 @@
+import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+
+export const GET: APIRoute = async ({ site }) => {
+  const posts = await getCollection('blog');
+  const baseURL = site?.href || 'https://formulafarma.com';
+  
+  // Páginas estáticas
+  const staticPages = [
+    '',
+    '/blog',
+    '/sobre-mi',
+    '/contacto',
+    '/aviso-legal',
+    '/privacidad',
+    '/cookies',
+  ];
+  
+  // Generar sitemap
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml"
+        xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+${staticPages.map(page => `  <url>
+    <loc>${baseURL}${page}</loc>
+    <changefreq>${page === '' ? 'daily' : 'weekly'}</changefreq>
+    <priority>${page === '' ? '1.0' : '0.8'}</priority>
+  </url>`).join('\n')}
+${posts
+  .sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
+  .map(post => `  <url>
+    <loc>${baseURL}/blog/${post.slug}</loc>
+    <lastmod>${post.data.date.toISOString().split('T')[0]}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>`).join('\n')}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+    },
+  });
+};
+
