e49170deab84fbcd08a588c025980cc1f963fc1a
Abel Eiras <abel.eiras@hotmail.com>
2025-12-15 21:23:59 +0100

    Add MDX support to Astro configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 import { remarkImageOptimize } from './src/plugins/remark-image-optimize.js';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://formulafarma.com',
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   markdown: {
     remarkPlugins: [remarkImageOptimize],
   },
